a1b5055214e3ba6512a8cc5bc13a6f1bf385fb56
cesarfabri <cesar.fabrii@gmail.com>
2022-08-13 14:31:22 -0300

    Prisma seed files

---
+++ files.ts
@@ -0,0 +1,25 @@
+// files seed
+export const files = 
+[
+  {
+    "name": "arquivo 1",
+    "type": "plain-text",
+    "size": 1,
+    "original_name": "file1.txt",
+    "path_url": "root/files/file1.txt"
+  },
+  {
+    "name": "arquivo 2",
+    "type": "plain-text",
+    "size": 2,
+    "original_name": "file2.txt",
+    "path_url": "root/files/file2.txt"
+  },
+  {
+    "name": "arquivo 3",
+    "type": "plain-text",
+    "size": 3,
+    "original_name": "file3.txt",
+    "path_url": "root/files/file3.txt"
+  },
+]
