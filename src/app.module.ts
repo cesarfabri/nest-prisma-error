cf927f9f63830970c86fa7b35611b3561561e354
cesarfabri <cesar.fabrii@gmail.com>
2022-08-13 15:31:33 -0300

    Criado resource files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
